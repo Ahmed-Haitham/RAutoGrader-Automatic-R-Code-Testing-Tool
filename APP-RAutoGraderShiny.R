50cc057e0bfcd313da2ad69a1a8c82b14b467fbf
Daniela Quintero Narvaez <daquina1110@hotmail.com>
2023-04-30 23:49:17 +0200

    First commit to replicate main - profile set up

--- APP-RAutoGraderShiny.R
+++ APP-RAutoGraderShiny.R
@@ -1,3 +1,37 @@
-install.packages("renv")
-library(renv)
-renv::init()
+# Set language to English
+Sys.setlocale(category = "LC_CTYPE", locale = "en_US.UTF-8")
+
+# Specify required packages
+required_packages <- c("dplyr", "ggplot2", "tidyr", "renv", "shiny")
+
+# Install missing packages
+missing_packages <- setdiff(required_packages, rownames(installed.packages()))
+if (length(missing_packages) > 0) {
+  install.packages(missing_packages)
+}
+
+# Load required packages
+lapply(required_packages, library, character.only = TRUE)
+
+#renv::restore()
+
+
+
+# Shiny App
+ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("Tab1", "whatever"),
+    tabPanel("Tab2", "blabla"),
+    tabPanel("Tab3", "blabla"),
+    tabPanel("Tab4", "blabla"),
+    tabPanel("Tab5", "blabla")
+  )
+  
+)
+
+server <- function(input, output) 
+  
+shiny::shinyApp(ui, server)
+
+
+
